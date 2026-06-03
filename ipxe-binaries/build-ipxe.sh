683305bdfd34db6cc46a69f494814ae109e6d488
Dmitry Tantsur <dtantsur@protonmail.com>
2026-06-03 19:04:55 +0200

    Improve bash style in ipxe-binaries
    Signed-off-by: Dmitry Tantsur <dtantsur@protonmail.com>

--- ipxe-binaries/build-ipxe.sh
+++ ipxe-binaries/build-ipxe.sh
@@ -5,18 +5,18 @@ set -euxo pipefail
 git clone https://github.com/ipxe/ipxe.git
 cd ipxe
 mkdir out
-git reset --hard "$IPXE_COMMIT_HASH"
+git reset --hard "${IPXE_COMMIT_HASH}"
 cd src
 
 # Build iPXE binaries based on architecture
-if [[ "$TARGETARCH" == "amd64" ]]; then
+if [[ "${TARGETARCH}" == "amd64" ]]; then
     NO_WERROR=1 make bin/undionly.kpxe bin-x86_64-efi/snponly.efi
     NO_WERROR=1 make CROSS=aarch64-linux-gnu- bin-arm64-efi/snponly.efi
-elif [[ "$TARGETARCH" == "arm64" ]]; then
+elif [[ "${TARGETARCH}" == "arm64" ]]; then
     NO_WERROR=1 make bin-arm64-efi/snponly.efi
     NO_WERROR=1 make CROSS=x86_64-linux-gnu- bin/undionly.kpxe bin-x86_64-efi/snponly.efi
 else
-    echo "ERROR: Unsupported build architecture: $TARGETARCH"
+    echo "ERROR: Unsupported build architecture: ${TARGETARCH}"
     exit 1
 fi
 
--- ipxe-binaries/prepare-ipxe.sh
+++ ipxe-binaries/prepare-ipxe.sh
@@ -14,15 +14,15 @@ dnf config-manager --set-disabled epel && \
 dnf install -y git-core make perl xz-devel
 
 # Install appropriate gcc binaries based on build architecture
-if [[ "$TARGETARCH" == "amd64" ]]; then
+if [[ "${TARGETARCH}" == "amd64" ]]; then
     # On x86_64, we need native gcc for x86 builds and cross-compiler for arm64
     dnf install -y gcc && \
     dnf install --enablerepo=epel -y gcc-aarch64-linux-gnu gcc-c++-aarch64-linux-gnu
-elif [[ "$TARGETARCH" == "arm64" ]]; then
+elif [[ "${TARGETARCH}" == "arm64" ]]; then
     # On arm64, we need native gcc for arm64 builds and cross-compiler for x86_64
     dnf install -y gcc && \
     dnf install --enablerepo=epel -y gcc-x86_64-linux-gnu gcc-c++-x86_64-linux-gnu
 else
-    echo "ERROR: Unsupported build architecture: $TARGETARCH"
+    echo "ERROR: Unsupported build architecture: ${TARGETARCH}"
     exit 1
 fi
